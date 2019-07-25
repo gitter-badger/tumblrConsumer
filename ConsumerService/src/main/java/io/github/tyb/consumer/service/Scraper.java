7298df5cb7538a32cd5644afa6c598698b9a4162
Taha YavuzBodur <tybodur@gmail.com>
2019-07-26 02:46:42 +0300

    1. worked on jsoup: like a web crawler, after get a page and parsed it get links and fetch those links' pages: ekşisözlükteki gündem alınarak her bir gündem maddesi için oradaki entryleri fav sayısına göre sıralayarak bir map'te tuttum. Bu desc sıralayıp en populer 10 entry'yi göstermek için kullanılabilir. Ayrıca tumblr'daki etiketleri indirip LDA vs. ya da lucene indexing ile oluştuurlmuş etiketlere göre sadece ilgi alanıma girenlerden belli popülerleri ya da tamamını göster diyebilirim. 1.1 Pagination eksik. 1.2 AJAX ile işlem yapan ya da javascript ile JSON getiren sayfaların parse edilmesi noktasında Selenium WebDriver ya da diğer tool'lara bakılacak. 1.3 jsoup ile POST da yapılabiliyor ya da header parametreleri de gönderilebiliyor, bunlara bak. 1.4 en uç senaryoda web sayfası HttpClient ile bir şekilde alındıktan sonra sadece html'i parse etmek için kullanılabilir. 1.5 benim tumblr ya da diğer içeriklerim html olacağından bunları hem rich text editor'de göstermek üzere html üzere saklarken hem de bazı durumlarda bununla html'i parse edebiliriz.
    2. pretty print senaryolarını tam uygulayamadım, ama reflection ile field'lara ulaşıp yazdırdım.
    
    3. Gson ya da Jackson farketmez, Circular reference içeren bir obje ile işlem yaptığımmızda Stackoverflowerror alıyoruz.
    
    4. array, list, set, map işlemlerinde java 8 lambda, functional ve stream API'yi kullandım. daha birçok olası senaryoda extensively kullanılacak.

--- ConsumerService/src/main/java/io/github/tyb/consumer/service/Scraper.java
+++ ConsumerService/src/main/java/io/github/tyb/consumer/service/Scraper.java
@@ -1,10 +1,20 @@
 package io.github.tyb.consumer.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 
 @Component
 public class Scraper {
@@ -12,14 +22,159 @@ public class Scraper {
     public void getEksiGundem() {
         try {
             Document doc = Jsoup.connect("https://eksisozluk.com/basliklar/gundem").get();
-            System.out.println(doc.title());
+            System.out.println("ana başlık: " + doc.title());
+
             Elements newsHeadlines = doc.select(".topic-list > li > a");
             for (Element headline : newsHeadlines) {
-                System.out.printf("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+                System.out.printf("%s\n\t%s", headline.absUrl("href"), headline.text());
+                System.out.println("");
+
+                Document doc2 = Jsoup.connect(headline.attr("abs:href"))
+                        .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                        .maxBodySize(0)
+                        .timeout(1000*5)
+                        .get();
+                System.out.println("gündem item başlığı: " + doc2.title());
+
+                //pretty print the object.
+                //System.out.println(new Gson().toJson(doc2));
+
+                //ObjectMapper mapper = new ObjectMapper();
+                //mapper.writerWithDefaultPrettyPrinter().writeValueAsString(doc2);
+                //System.out.println(new JSONObject(stats).toString(2));
+
+                //Arrays.toString(((ArrayList<?>)doc2).entrySet().toArray())
+                //Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+                //gson.toJson(doc2);
+                System.out.println(printObject(doc2));
+
+                Elements entries = doc2.select("ul#entry-item-list li");
+                System.out.println("entries: " + printObject(entries));
+
+                Map<Integer, String> entryMap = new HashMap<Integer, String>();
+                for (Element entry : entries) {
+                    System.out.println("fav:" + entry.attr("data-favorite-count"));
+                    System.out.println("entry text: " + entry.text());
+                    System.out.println("entry: " + printObject(entry));
+
+                    //We add numbers to the list. Autoboxing wraps primitive int types to the Integer objects.
+                    entryMap.put(Integer.parseInt(entry.attr("data-favorite-count")), entry.text());
+                    //String objStr = new Gson().toJson(entry);
+                    //System.out.println("pretty printing object: " + objStr);
+                }
+
+                // let's sort this map by values first
+                Map<Integer, String> sorted = entryMap
+                        .entrySet()
+                        .stream()
+                        .sorted(comparingByKey())
+                        .collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                        LinkedHashMap::new));
+
+                System.out.println("map after sorting by values: " + sorted);
+
+                /*
+                // above code can be cleaned a bit by using method reference
+                sorted = entryMap
+                        .entrySet()
+                        .stream()
+                        .sorted(comparingByValue())
+                        .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                                        LinkedHashMap::new));
+                */
+                // now let's sort the map in decreasing order of value
+                sorted = entryMap
+                        .entrySet()
+                        .stream()
+                        .sorted(Collections.reverseOrder(comparingByKey()))
+                        .collect(
+                                toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                                        LinkedHashMap::new));
+
+                 //traversing a list:
+                 /*
+                 List<String> martialArts = new ArrayList<>();
+                 martialArts.forEach((e) -> System.out.printf("%s ", e));
+
+                  */
+
+                 //string operations
+                /*
+                Map<String, Integer> wordCount = new HashMap<>();
+
+                String fileName = "src/resources/thermopylae.txt";
+
+                List<String> lines = Files.readAllLines(Paths.get(fileName),
+                        StandardCharsets.UTF_8);
+
+                for (String line : lines) {
+
+                    String[] words = line.split("\\s+");
+
+                    for (String word : words) {
+
+                        if (word.endsWith(".") || word.endsWith(",")) {
+                            word = word.substring(0, word.length()-1);
+                        }
+
+                        if (wordCount.containsKey(word)) {
+                            wordCount.put(word, wordCount.get(word) + 1);
+
+                        } else {
+                            wordCount.put(word, 1);
+                        }
+                    }
+                }
+
+                for (String key : wordCount.keySet()) {
+                    System.out.println(key + ": " + wordCount.get(key));
+                }
+                */
+
+                //java8 map collect
+                /*
+                List<Staff> staff = Arrays.asList(
+                new Staff("mkyong", 30, new BigDecimal(10000)),
+                new Staff("jack", 27, new BigDecimal(20000)),
+                new Staff("lawrence", 33, new BigDecimal(30000)));
+                List<String> collect = staff.stream().map(x -> x.getName()).collect(Collectors.toList());
+
+                 List<Map<String, String>> listOfMaps;
+                List<String> valuesMatchingKey = listOfMaps.stream()
+                    .filter(map -> map.containsKey("Key"))
+                    .map(map -> map.get("Key"))
+                    .collect(Collectors.toList());
+
+
+                 List<Map<String, String>> tx = l.stream().map(m -> m.entrySet().stream()
+                    .filter(map -> map.getKey().equals("x") || map.getKey().equals("z"))
+                    .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue())))
+                .collect(Collectors.toList());
+                List<Map<String, String>> tx = l.stream()
+                    .map(m -> Stream.of("x", "y")
+                                    .filter(m::containsKey).collect(Collectors.toMap(key->key, m::get)))
+                    .collect(Collectors.toList());
+                 */
             }
         }
         catch (Exception e) { e.printStackTrace();}
 
+    }
+
+    public static <T> String printObject(T t) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Field field : t.getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+
+            try {
+                sb.append(field.getName()).append(": ").append(field.get(t)).append('\n');
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 
 }
